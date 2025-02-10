373090dcb5adf3652959ec974b57ceae2de2b31a
Nico Weber <thakis@chromium.org>
2025-02-10 18:16:54 -0500

    add more bypass tests
    test interaction of bypass with:
    * layers
    * termall
    * segsym
    
    not all passing yet
    
    created with:
    
        ~/Downloads/jasper-build/src/app/jasper --input \
        Tests/LibGfx/*-inputs/*2000/kakadu-lossless-rgba-u8-prog1*.jp2 \
        --output Tests/LibGfx/test-inputs/jpeg2000/jasper-rgba-u8-cbstyle-01-bypass-layers.jp2 \
        -O lazy -O ilyrrates=0.1,0.5,1
    
        ~/Downloads/jasper-build/src/app/jasper --input \
        Tests/LibGfx/*-inputs/*2000/kakadu-lossless-rgba-u8-prog1*.jp2 \
        --output Tests/LibGfx/test-inputs/jpeg2000/jasper-rgba-u8-cbstyle-05-bypass-termall.jp2 \
        -O lazy -O termall
    
        ~/Downloads/jasper-build/src/app/jasper --input \
        Tests/LibGfx/*-inputs/*2000/kakadu-lossless-rgba-u8-prog1*.jp2 \
        --output Tests/LibGfx/test-inputs/jpeg2000/jasper-rgba-u8-cbstyle-33-bypass-segsym.jp2 \
        -O lazy -O segsym

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -696,14 +696,17 @@ TEST_CASE(test_jpeg2000_decode)
         TEST_INPUT("jpeg2000/kakadu-lossless-rgba-u8-prog1-layers1-res6-mct.jp2"sv),
         TEST_INPUT("jpeg2000/openjpeg-lossless-rgba-u8-prog0-tile3x2-cblk4x16-tp3-layers3-res2-mct.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-01-bypass.jp2"sv),
+        // TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-01-bypass-layers.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-02-resetprob.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-04-termall.jp2"sv),
+        // TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-05-bypass-termall.jp2"sv), // XXX
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-04-termall-layers.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-06-resetprob-termall.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-08-vcausal.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-16-pterm.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-32-segsym.jp2"sv),
-        TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-63-all.jp2"sv),
+        TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-33-bypass-segsym.jp2"sv),
+        // TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-63-all.jp2"sv), // XXX
         TEST_INPUT("jpeg2000/jasper-tile3x2-res5.jp2"sv),
         TEST_INPUT("jpeg2000/openjpeg-lossless-rgba-u8-prog0-SOP.jp2"sv),
         TEST_INPUT("jpeg2000/openjpeg-lossless-rgba-u8-prog0-EPH.jp2"sv),
