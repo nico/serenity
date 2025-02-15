5f79f0e0c3f983c641f825c99fe7a3d394b87b44
Nico Weber <thakis@chromium.org>
2025-02-14 20:16:26 -0500

    Tests: Add another JPEG2000 bypass code block style test image
    What's interesting about this one is that it has the the first 10
    passes spread out over 3 layers, so that the middle layer contains
    a single segment that continues the first segment from the first
    layer, and is further continued in the third layer. The second
    layer contains not a single complete segment.
    
    The code handles this fine, but this would've caught a bug in
    a refactoring that I accidentally introduced locally and that
    the tests didn't catch until I added this test case here.
    
    Generated with `jasper -O lazy -O ilyrrates=0.002,0.009,1`.

--- Tests/LibGfx/TestImageDecoder.cpp
+++ Tests/LibGfx/TestImageDecoder.cpp
@@ -697,6 +697,7 @@ TEST_CASE(test_jpeg2000_decode)
         TEST_INPUT("jpeg2000/openjpeg-lossless-rgba-u8-prog0-tile3x2-cblk4x16-tp3-layers3-res2-mct.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-01-bypass.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-01-bypass-layers.jp2"sv),
+        TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-01-bypass-finer-layers.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-02-resetprob.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-04-termall.jp2"sv),
         TEST_INPUT("jpeg2000/jasper-rgba-u8-cbstyle-05-bypass-termall.jp2"sv),
