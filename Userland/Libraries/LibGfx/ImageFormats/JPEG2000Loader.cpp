706d55c53856c910750e0a8635146ba76867e0c6
Nico Weber <thakis@chromium.org>
2025-03-20 15:53:27 -0400

    LRCP -> SyncGenerator

--- Userland/Libraries/LibGfx/ImageFormats/JPEG2000Loader.cpp
+++ Userland/Libraries/LibGfx/ImageFormats/JPEG2000Loader.cpp
@@ -10,6 +10,7 @@
 #include <AK/Debug.h>
 #include <AK/Enumerate.h>
 #include <AK/MemoryStream.h>
+#include <AK/SyncGenerator.h>
 #include <LibGfx/ImageFormats/ISOBMFF/JPEG2000Boxes.h>
 #include <LibGfx/ImageFormats/ISOBMFF/Reader.h>
 #include <LibGfx/ImageFormats/JPEG2000Loader.h>
@@ -658,29 +659,40 @@ public:
     virtual ProgressionData next() override;
 
 private:
-    Function<int(int resolution_level, int component)> m_number_of_precincts;
-    ProgressionData m_next {};
-    ProgressionData m_end {};
+    SyncGenerator<ProgressionData> generator();
+
+    Optional<ProgressionData> m_next;
+    int m_layer_count { 0 };
+    int m_max_number_of_decomposition_levels { 0 };
+    int m_component_count { 0 };
+    Function<int(int resolution_level, int component)> m_precinct_count;
+    SyncGenerator<ProgressionData> m_generator;
 };
 
 LayerResolutionLevelComponentPositionProgressionIterator::LayerResolutionLevelComponentPositionProgressionIterator(int number_of_layers, int max_number_of_decomposition_levels, int component_count, Function<int(int resolution_level, int component)> number_of_precincts)
-    : m_number_of_precincts(move(number_of_precincts))
+    : m_layer_count(number_of_layers)
+    , m_max_number_of_decomposition_levels(max_number_of_decomposition_levels)
+    , m_component_count(component_count)
+    , m_precinct_count(move(number_of_precincts))
+    , m_generator(generator())
 {
-    m_end.layer = number_of_layers;
-    m_end.resolution_level = max_number_of_decomposition_levels + 1;
-    m_end.component = component_count;
-    m_end.precinct = m_number_of_precincts(m_next.resolution_level, m_next.component);
+    m_next = m_generator.next();
 }
 
 bool LayerResolutionLevelComponentPositionProgressionIterator::has_next() const
 {
-    return m_next != ProgressionData { m_end.layer, 0, 0, 0 };
+    return m_next.has_value();
 }
 
 ProgressionData LayerResolutionLevelComponentPositionProgressionIterator::next()
 {
-    ProgressionData current_data = m_next;
+    auto result = m_next;
+    m_next = m_generator.next();
+    return result.value();
+}
 
+SyncGenerator<ProgressionData> LayerResolutionLevelComponentPositionProgressionIterator::generator()
+{
     // B.12.1.1 Layer-resolution level-component-position progression
     // "for each l = 0,..., L – 1
     //      for each r = 0,..., Nmax
@@ -690,32 +702,11 @@ ProgressionData LayerResolutionLevelComponentPositionProgressionIterator::next()
     //  Here, L is the number of layers and Nmax is the maximum number of decomposition levels, N_L, used in any component of the tile."
     // FIXME: This always iterates up to Nmax, instead of just N_l of each component. That means several of the iteration results will be invalid and skipped.
     // (This is a performance issue, not a correctness issue.)
-
-    ++m_next.precinct;
-    if (m_next.precinct < m_end.precinct)
-        return current_data;
-
-    m_next.precinct = 0;
-    ++m_next.component;
-    if (m_next.component < m_end.component) {
-        m_end.precinct = m_number_of_precincts(m_next.resolution_level, m_next.component);
-        return current_data;
-    }
-
-    m_next.component = 0;
-    ++m_next.resolution_level;
-    if (m_next.resolution_level < m_end.resolution_level) {
-        m_end.precinct = m_number_of_precincts(m_next.resolution_level, m_next.component);
-        return current_data;
-    }
-
-    m_next.resolution_level = 0;
-    m_end.precinct = m_number_of_precincts(m_next.resolution_level, m_next.component);
-
-    ++m_next.layer;
-    VERIFY(m_next.layer < m_end.layer || !has_next());
-
-    return current_data;
+    for (int l = 0; l < m_layer_count; ++l)
+        for (int r = 0; r <= m_max_number_of_decomposition_levels; ++r)
+            for (int i = 0; i < m_component_count; ++i)
+                for (int k = 0; k < m_precinct_count(r, i); ++k)
+                    co_yield ProgressionData { l, r, i, k };
 }
 
 // B.12.1.2 Resolution level-layer-component-position progression
